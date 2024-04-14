02b1fe7dffd64da965107949d33f806595e4e7db
mborik128 <mborik@users.sourceforge.net>
2024-04-14 18:43:37 +0200

    depackblock script: kind of profiler

--- _utils/depackblock.js
+++ _utils/depackblock.js
@@ -5,6 +5,8 @@ Z80 = require("./z80").default;
 if (process.argv.length < 5)
 	return console.error('few arguments, run "node depackblock.js {zxblock.bin} {loadAddr,runAddr,stackAddr,saveFrom,saveTo} {output.bin}"!');
 
+process.removeAllListeners('warning');
+
 //-----------------------------------------------------------------------------
 
 const rom = fs.readFileSync(path.parse(process.argv[1]).dir + '/48.rom')
@@ -18,6 +20,8 @@ const [ loadAddr, runAddr, stackAddr, saveFrom, saveTo ] =
 	});
 
 const mem = new Buffer(64 * 1024);
+let wwMin = Infinity;
+let wwMax = -1;
 
 mem.set(rom);
 mem.set(block, loadAddr);
@@ -30,13 +34,23 @@ mem.writeUInt8(0xF3, 6);
 mem.writeUInt8(0x76, 7);
 
 const cpu = Z80({
-	mem_read: (addr) => mem.readUInt8(addr & 0xffff),
-	mem_write: (addr, byte) => mem.writeUInt8(byte & 0xff, addr & 0xffff),
 	io_read: () => 0,
-	io_write: () => void 0
+	io_write: () => void 0,
+	mem_read: (addr) => mem.readUInt8(addr & 0xffff),
+	mem_write: (addr, byte) => {
+		addr &= 0xffff;
+		if (addr >= 16384) {
+			wwMin = Math.min(wwMin, addr);
+			wwMax = Math.max(wwMax, addr);
+		}
+
+		mem.writeUInt8(byte & 0xff, addr & 0xffff);
+	}
 });
 
 cpu.reset();
 while (cpu.run_instruction() > 0);
+
+console.log(`Writing window within working area is <${wwMin}, ${wwMax}>.`);
 
 fs.writeFileSync(path.normalize(process.argv[4]), mem.slice(saveFrom, saveTo), { flag: 'w' });
