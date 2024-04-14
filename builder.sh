7f6e0698b8cc40466719d5c41908497b06c54946
mborik128 <mborik@users.sourceforge.net>
2024-04-14 14:58:18 +0200

    regression kernel as builder

---
+++ builder.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# demo builder (c) 2019-2024 mborik/SinDiKat
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Utils required in PATH to successful build:
+# latest `sjasmplus` from https://github.com/z00m128/sjasmplus
+# `zx0` from https://github.com/einar-saukas/ZX0
+
+ASM="sjasmplus"
+OUTPUT="INFERNO.tap"
+
+function ASM() {
+	FULLPATH=$(realpath $1)
+	shift
+	sjasmplus --nologo --fullpath --color=off --msg=war $FULLPATH $@
+}
+function PAK() {
+	rm -f $2
+	zx0 -f $1 &> /dev/null
+	mv -f "$1.zx0" $2
+}
+
+cd build
+rm -f ${OUTPUT}
+
+# Every part is compiled using this cmds pattern:
+#
+#> cd ../part
+#> ASM part.a80 -DisFX --lst=part.lst
+#> PAK final.bin final.pak
+
+# banks composition
+cd ..
+rm -f bank*
+ASM pg0.a80 --lst=main/pg0.lst --exp=main/pg0.inc
+ASM pg1.a80 --lst=main/pg1.lst --exp=main/pg1.inc
+PAK bank1 bank1.pak
+ASM pg3.a80 --lst=main/pg3.lst --exp=main/pg3.inc
+PAK bank3 bank3.pak
+ASM pg4.a80 --lst=main/pg4.lst --exp=main/pg4.inc
+PAK bank4 bank4.pak
+ASM pg6.a80 --lst=main/pg6.lst --exp=main/pg6.inc
+PAK bank6 bank6.pak
+ASM pg7.a80 --lst=main/pg7.lst --exp=main/pg7.inc
+PAK bank7 bank7.pak
+
+cd main
+PAK loading.scr loading.pak
+ASM main.a80 --lst=main.lst --exp=constants.inc
+PAK final.bin final.pak
+ASM loader.a80 -DOUTPUT="\"../build/${OUTPUT}\"" --lst=loader.lst
---
+++ clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# demo cleaner (c) 2019-2023 mborik/SinDiKat
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+rm -f bank*
+
+pushd build
+rm -f *.tap
+popd
+
+subdirs=(
+	main
+)
+
+for D in "${subdirs[@]}"; do
+	pushd "${D}"
+	rm -f final.bin *.lst *.sna *.tap
+	find . -name "*.pak" -type f -delete
+	popd
+done
