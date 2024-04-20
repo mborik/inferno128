d21ef763aebf17a365cefcca4c00efa8173a851c
mborik128 <mborik@users.sourceforge.net>
2024-04-20 20:54:45 +0200

    level6 data done, translated but it wont fit Houston, we have a problem! :/

--- builder.sh
+++ builder.sh
@@ -63,6 +63,12 @@ PAK final.bin final.pak
 PAK loading.scr loading.pak
 PAK music.stc music.pak
 
+cd ../level6
+ASM level6.a80 -DisFX --lst=level6.lst
+PAK final.bin final.pak
+PAK loading.scr loading.pak
+PAK music.stc music.pak
+
 # banks composition
 cd ..
 rm -f bank*
