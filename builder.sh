64116128b95d5ed4f5f99ac3a5623f0ed13cb7c1
mborik128 <mborik@users.sourceforge.net>
2024-04-20 15:01:01 +0200

    level5 data done, translated and included into pages

--- builder.sh
+++ builder.sh
@@ -57,6 +57,12 @@ PAK final.bin final.pak
 PAK loading.scr loading.pak
 PAK music.stc music.pak
 
+cd ../level5
+ASM level5.a80 -DisFX --lst=level5.lst
+PAK final.bin final.pak
+PAK loading.scr loading.pak
+PAK music.stc music.pak
+
 # banks composition
 cd ..
 rm -f bank*
